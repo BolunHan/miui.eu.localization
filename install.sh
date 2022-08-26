2502daae6b492a83bab34a85d4ca603e4abe908d
Bolun Han <Bolun.Han@outlook.com>
2022-08-27 01:31:36 +0800

    init commit

--- install.sh
+++ install.sh
@@ -1,40 +1,11 @@
-##########################################################################################
-#
-# Magisk
-# by topjohnwu
-# 
-# This is a template zip for developers
-#
-##########################################################################################
-##########################################################################################
-# 
-# Instructions:
-# 
-# 1. Place your files into system folder (delete the placeholder file)
-# 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (common/config.sh)
-# 4. For advanced features, add shell commands into the script files under common:
-#    post-fs-data.sh, service.sh
-# 5. For changing props, add your additional/modified props into common/system.prop
-# 
-##########################################################################################
-
-##########################################################################################
-# Defines
-##########################################################################################
-
-# NOTE: This part has to be adjusted to fit your own needs
-
-# This will be the folder name under /magisk
-# This should also be the same as the id in your module.prop to prevent confusion
-MODID=mipay
+MODID=miui.propreset
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
 AUTOMOUNT=true
 
 # Set to true if you need to load system.prop
-PROPFILE=true
+PROPFILE=false
 
 # Set to true if you need post-fs-data script
 POSTFSDATA=false
@@ -49,33 +20,17 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "      Mipay Module v0.1.0      "
-  ui_print "*******************************"
-  ui_print "                               "
-  ui_print "钱包如果能调出刷卡界面但是刷卡无反应"
-  ui_print "在系统设置中将默认钱包改为HCE再改回小米钱包。"
+  ui_print "********************************"
+  ui_print " MIUI PropReset Module v0.1.0 "
+  ui_print "********************************"
 }
 
 ##########################################################################################
 # Replace list
 ##########################################################################################
 
-# List all directories you want to directly replace in the system
-# By default Magisk will merge your files with the original system
-# Directories listed here however, will be directly mounted to the correspond directory in the system
-
-# You don't need to remove the example below, these values will be overwritten by your own list
-# This is an example
 REPLACE="
-/system/app/NextPay
 "
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-# NOTE: This part has to be adjusted to fit your own needs
 
 set_permissions() {
   # Default permissions, don't remove them
@@ -95,6 +50,11 @@ set_permissions() {
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
 }
 
-rm -rf /data/misc_de/0/apexdata/com.android.permission/runtime-permissions.xml
-ui_print "- Extracting module files"
-unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+ui_print "- overriding system.prop"
+# echo "" >> $MODPATH/system.prop
+# echo "ro.product.mod_device=xiaomieu" >> $MODPATH/system.prop
+# echo "ro.miui.region=CN" >> $MODPATH/system.prop
+# echo "ro.com.google.ime.theme_dir=" >> $MODPATH/system.prop
+# echo "ro.com.google.ime.theme_file=" >> $MODPATH/system.prop
+# echo "" >> $MODPATH/system.prop
+unzip -p "$ZIPFILE" 'common/system.prop' >$MODPATH/system.prop
